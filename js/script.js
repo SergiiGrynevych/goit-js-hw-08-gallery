7941e0fdf292837fa31e3277068d666a849bd772
Sergii Grynevych <grinevich.sergey.official@gmail.com>
2020-11-22 20:54:06 +0200

    all tasks is done. Gallery complete

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 import galleryItems from './gallery-items.js';
 
 const gallery = document.querySelector(".js-gallery");
+const btnToCloseModal = document.querySelector(".lightbox__button");
+const modal = document.querySelector(".js-lightbox");
+const modalImage = document.querySelector(".lightbox__image");
+const overlay = document.querySelector(".lightbox__overlay");
 
 gallery.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
+gallery.addEventListener("click", modalIsOpen);
 
 function galleryMarkup(img) {
     return img.map(({preview, original, description}) => {
@@ -21,6 +26,44 @@ function galleryMarkup(img) {
       </li>`;
     }).join("");
 }
+
+function modalIsOpen(event){
+  event.preventDefault();
+  
+  if(event.target.nodeName !== "IMG"){
+    return;
+  }
+
+  modal.classList.add("is-open");
+  modalImage.src = event.target.dataset.source;
+  modalImage.alt = event.target.alt;
+  btnToCloseModal.addEventListener("click", toCloseModal);
+  overlay.addEventListener("click", toCloseModalByOverlay);
+  document.addEventListener("keydown", toCloseModalByEsc);
+  console.dir(event);
+}
+
+function toCloseModal() {
+  modal.classList.remove("is-open");
+  modalImage.src = '';
+  modalImage.alt = '';
+  document.removeEventListener("keydown", toCloseModalByEsc);
+  overlay.removeEventListener("click", toCloseModalByOverlay);
+}
+
+function toCloseModalByOverlay(event){
+  if(event.target === event.currentTarget){
+    toCloseModal(event);
+  }
+}
+
+function toCloseModalByEsc(event) {
+  if(event.code === "Escape"){
+   toCloseModal();
+ }
+}
+
+
 
 
 
